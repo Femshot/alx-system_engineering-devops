036ebbc1ff8bfe0a2ab9696fb312f1abbd5095c8
Awodele Femi <awodelefemi@yahoo.com>
2023-10-10 13:28:48 -0700

    Fixed pycodestyle errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ How many Subs? """
-from requests import get 
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API
-        
+
         Returns the total number of subscribers in a given subreddit
     """
     header = {'User-Agent': 'Google Chrome Version 117.0.5938.150'}
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     response = get(url, headers=header, allow_redirects=False)
     reddits = response.json()
 
-     try:
+    try:
         return reddits.get('data').get('subscribers')
     except Exception:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """ How many Subs? """
-from requests import get 
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API
-        
+
         Prints the titles of the first 10 hot posts
         listed for a given subreddit
     """
     header = {'User-Agent': 'Google Chrome Version 117.0.5938.150'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    response = get(url, headers=header, params=params, allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}/hott.json'.format(subreddit)
+    response = get(url, headers=header, allow_redirects=False)
     reddits = response.json()
 
-     try:
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
+    try:
+        return reddits.get('data').get('subscribers')
     except Exception:
-        return None
+        return 0
