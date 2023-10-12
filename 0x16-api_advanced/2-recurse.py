3adbffc20e40e9fc26a50dd7e8ac48a56e2be406
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-10-12 14:47:24 +0100

    Modifications to task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 from requests import get
 
 
-def number_of_subscribers(subreddit):
+def recurse(subreddit, hot_list=[]):
     """ Recursive function that queries the Reddit API
 
         and returns a list containing the titles of all hot articles
